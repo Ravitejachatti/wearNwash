ad194926c427dcd02b2b2416f533f063a44bde18
sai kiran <saikiran@sais-MacBook-Air.local>
2024-08-20 12:30:25 +0530

    added-compose-redux

--- Redux/store.js
+++ Redux/store.js
@@ -1,17 +1,17 @@
+import { legacy_createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import  thunk  from 'redux-thunk';
 
+import { reducer as appReducer } from './App/reducer';
+import { reducer as authReducer } from './Auth/reducer';
 
+const rootReducer = combineReducers({
+  app: appReducer,
+  auth: authReducer,
+});
 
-import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-import {reducer as appReducer}  from "./App/reducer";
-
-import {reducer as authReducer}  from "./Auth/reducer";
-
-import { thunk } from "redux-thunk";
-
-const rootReducer = combineReducers({appReducer,authReducer});
-
-export  const store =  legacy_createStore(rootReducer, applyMiddleware(thunk))
-
-
-
+export const store = legacy_createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
