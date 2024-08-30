31862ce02935086817ad32d71e480c05451db46a
sai kiran <saikiran@sais-MacBook-Air.local>
2024-08-30 22:23:23 +0530

    axios-change

--- Redux/Auth/action.js
+++ Redux/Auth/action.js
@@ -2,20 +2,20 @@ import axios from "axios";
 import * as types from "./actionTypes";
 
 export const postRegister = (payload) => (dispatch) => {
-  console.log("Action payload:", payload); // Log payload to check data
+  console.log("Action payload:", payload); 
 
   dispatch({ type: types.USER_REGISTER_REQUEST });
 
   return axios
     .post("http://10.0.2.2:2347/api/user/register", payload)
     .then((res) => {
-      console.log("Server response:", res); // Log response for debugging
+      console.log("Server response:", res); 
       dispatch({ type: types.USER_REGISTER_SUCCESS, payload: res.data });
-      return res.data; // Return response data for chaining
+      return res.data; 
     })
     .catch((err) => {
-      console.log("Error occurred in postRegister:", err); // Log error for debugging
+      console.log("Error occurred in postRegister:", err); 
       dispatch({ type: types.USER_REGISTER_FAILURE, payload: err });
-      throw err; // Rethrow error to catch it in the component
+      throw err; 
     });
 };
